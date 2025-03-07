7a95361f0d3561e651e0d215a822b5e9e47fbc02
Omer Faruk Istikbal <ofarukistikbal@hotmail.com>
2025-03-07 11:36:27 +0300

    Add ProductService

---
+++ eCommerce.ProductService/Program.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Newtonsoft.Json;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHealthChecks();
+
+var app = builder.Build();
+
+app.UseHealthChecks("/health", new HealthCheckOptions
+{
+    // This will ensure the response is in JSON format.
+    ResponseWriter = async (context, report) =>
+    {
+        var result = new
+        {
+            status = report.Status.ToString(),
+            checks = report.Entries.Select(entry => new
+            {
+                name = entry.Key,
+                status = entry.Value.Status.ToString(),
+                description = entry.Value.Description
+            })
+        };
+
+        // Set the response content type to application/json
+        context.Response.ContentType = "application/json";
+
+        // Write the JSON response
+        await context.Response.WriteAsync(JsonConvert.SerializeObject(result));
+    }
+});
+
+app.UseHttpsRedirection();
+app.MapHealthChecks("/health");
+app.Run();
+
