d72c3ea6b9822e7276533a04f637c8655aa3846e
Omer Faruk Istikbal <ofarukistikbal@hotmail.com>
2025-03-09 12:30:38 +0300

    Add ProductService Proto

--- eCommerce.ProductService/Data/ApplicationDbContext.cs
+++ eCommerce.ProductService/Data/ApplicationDbContext.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using eCommerce.ProductService.Data.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace eCommerce.ProductService.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public DbSet<Product> Products { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
         }
---
+++ eCommerce.ProductService/Services/V1/ProductService.cs
@@ -0,0 +1,36 @@
+﻿using eCommerce.ProductService.Data;
+using eCommerce.ProductService.Data.Models;
+using eCommerce.ProductService.Protos.V1;
+using Grpc.Core;
+
+namespace eCommerce.ProductService.Services.V1
+{
+    public class ProductService : Protos.V1.ProductService.ProductServiceBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public override async Task<CreateProductResponse> CreateProduct(CreateProductRequest request, ServerCallContext context)
+        {
+            var product = new Product
+            {
+                Name = request.Name,
+                Description = request.Description,
+                Price = (decimal)request.Price,
+                Stock = request.Stock,
+            };
+
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+
+            return new CreateProductResponse
+            {
+                Success = true,
+            };
+        }
+    }
+}
