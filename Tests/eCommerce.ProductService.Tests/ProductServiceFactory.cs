3f8ad7db3b357c0843ff6f1d1779f3aa473a62f7
Omer Faruk Istikbal <ofarukistikbal@hotmail.com>
2025-03-09 12:53:15 +0300

    Add ProductService Test Project

---
+++ Tests/eCommerce.ProductService.Tests/ProductServiceFactory.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using eCommerce.ProductService.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Testcontainers.PostgreSql;
+
+namespace eCommerce.ProductService.Tests
+{
+    class ProductServiceFactory : WebApplicationFactory<Program>, IAsyncLifetime
+    {
+        private readonly PostgreSqlContainer _dbContainer;
+
+        public ProductServiceFactory()
+        {
+            _dbContainer = new PostgreSqlBuilder()
+                .WithDatabase("testdb")
+                .WithUsername("testuser")
+                .WithPassword("testpassword")
+                .WithImage("postgres:17")
+                .Build();
+        }
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureTestServices(services =>
+            {
+                services.AddScoped<ProductService.Services.V1.ProductService>();
+
+                services.RemoveAll(typeof(DbContextOptions<ApplicationDbContext>));
+                services.AddDbContext<ApplicationDbContext>(options =>
+                {
+                    options.UseNpgsql(_dbContainer.GetConnectionString());
+                });
+
+                using var scope = services.BuildServiceProvider().CreateScope();
+                var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                dbContext.Database.EnsureCreated();
+            });
+        }
+
+        public async Task InitializeAsync()
+        {
+            await _dbContainer.StartAsync();
+        }
+
+        public async Task DisposeAsync()
+        {
+            await _dbContainer.DisposeAsync();
+        }
+    }
+}
